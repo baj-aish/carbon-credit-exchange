5b8ef0af6d02934d38ba36f8b249c2f684527213
baj-aish <amitbajpai9555680157@gmail.com>
2025-12-07 21:00:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -199,7 +199,7 @@ document.getElementById("loginForm").addEventListener("submit", async e => {
   if (!name) return;
 
   try {
-    const res = await fetch("http://localhost:5000/api/login", {
+    const res = await fetch("https://carbon-credit-exchange-backend.onrender.com/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name, role })
@@ -362,3 +362,4 @@ document.getElementById("calcLandBtn").addEventListener("click", () => {
 // ---------- initial ----------
 updateAuthUI();
 renderFeed();
+
