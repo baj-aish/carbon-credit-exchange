3f79ee15d674dea37cb620f720242b85e06a595b
baj-aish <amitbajpai9555680157@gmail.com>
2025-12-20 01:13:25 +0530

    Create firebase.js

---
+++ backend/firebase.js
@@ -0,0 +1,12 @@
+import admin from "firebase-admin";
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FB_PROJECT_ID,
+    clientEmail: process.env.FB_CLIENT_EMAIL,
+    privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n")
+  })
+});
+
+export const db = admin.firestore();
+export const auth = admin.auth();
